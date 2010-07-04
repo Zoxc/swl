a1b361be5504188b7a1fa792bbc1871daefece09
Zoxc <zoxc32@gmail.com>
2010-07-04 04:06:33 +0200

    swl: Xlib backend now compiles.

--- swl-xlib.h
+++ swl-xlib.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "X11/Xlib.h"
 #include "X11/keysym.h"
 
 /*
  * Key type definition
  */
-typedef size_t swl_key_t;
+typedef KeySym swl_key_t;
 
 #define SWLK_UP XK_Up
 #define SWLK_DOWN XK_Down
@@ -14,3 +15,4 @@ typedef size_t swl_key_t;
 
 #define SWLR_ERROR_PLATFORM_XLIB_DISPLAY SWLR_PLATFORM(0)
 #define SWLR_ERROR_PLATFORM_XLIB_VISUAL SWLR_PLATFORM(1)
+
